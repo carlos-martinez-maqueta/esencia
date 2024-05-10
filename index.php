92b19643b835f78eaf8e23c96a157fa95dd1afe4
cmartinezcsti <cmartinez@csticorp.biz>
2024-05-10 00:26:44 -0500

    Home

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VILLA ESENCIA TULUM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+  </head>
+  <style>
+
+  </style>
+  <body>
+    <section class="section_banner">
+        <div class="cont_banner">
+            <div class="fondo_banner">
+                <div class="menu_banner">
+                    <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                        <img src="assets/img/menu.svg" alt="">
+                    </a>
+                    <div class="nav_iz offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">MENÚ</h5>
+                            <button type="button" class="btn" data-bs-dismiss="offcanvas" aria-label="Close">
+                                <img src="assets/img/close.svg" alt="">
+                            </button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul>
+                                <li><a href="">HOME</a></li>
+                                <li><a href="">BOOK NOW</a></li>
+                                <li><a href="">ABOUT US</a></li>
+                                <li><a href="">SERVICES</a></li>
+                            </ul>
+                        </div>
+                        <div class="offcanvas-bottom">
+                            <ul>
+                                <li><a href="">LOGIN</a></li>
+                                <li><a href="">REGISTER</a></li>
+                            </ul>
+                            <div class="d-flex lenguaje">
+                                <div class="w-50"><a href="">EN <img src="assets/img/en.svg" alt="" class="mx-3"></a></div>
+                                <div class="w-50"><a href="">ES <img src="assets/img/es.svg" alt="" class="mx-3"></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn_boook_banner">
+                    <a data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">BOOK NOW <img src="assets/img/btn-book-now.svg" alt=""></a>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                        <div class="offcanvas-header">
+                            <!-- <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5> -->
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                           <h3>WELCOME TO ESENCIA</h3>
+
+                           <p>Enjoy your stay at <br> Esencia Tulum</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="assets/img/banner.png" class="img_banner" alt="">
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
