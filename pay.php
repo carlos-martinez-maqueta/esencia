0e8dad0e9cef443004f00f3b9b4ca7f51c8b3c46
cmartinezcsti <cmartinez@csticorp.biz>
2024-05-23 14:26:31 -0500

    integration pay front index.js

--- pay.php
+++ pay.php
@@ -1,41 +1,29 @@
 <?php
-
 include 'dashboard/config/conexion.php';
 include 'dashboard/class/folder.php';
 include 'dashboard/class/user.php';
-
-$aboutFolderObj = Folder::getFolderAbout();
-$equipmentFolderObj = Folder::getFolderEquipment();
-
-
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>VILLA ESENCIA TULUM</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-  <link rel="stylesheet" type="text/css" href="assets/css/header-all.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/events.css">
+  <head>
+    <meta charset="utf-8" />
+    <title>Accept a payment</title>
+    <meta name="description" content="A demo of a payment on Stripe" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/header-all.css">
   <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
-</head>
- 
+  <link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="index.js" defer></script>
+  </head>
+  <body>
+    <?php include 'app/header-all.php'; ?>
+    <!-- Display a payment form -->
+      <div id="checkout" class="mt-5 p-5">
+        <!-- Checkout will insert the payment form here -->
+      </div>
 
-<body style="background-color: #ffffff;">
-
-  <?php include 'app/header-all.php'; ?>
-
-  <main>
-
-  </main>
- 
-  <?php include 'app/footer.php'; ?>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
+      <?php include 'app/footer.php'; ?>  
+  </body>
 </html>
